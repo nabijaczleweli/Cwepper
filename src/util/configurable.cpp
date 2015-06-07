f1fa8edab3de472dfaaf822d88834922310f8f5a
nabijaczleweli <nabijaczleweli@gmail.com>
2015-06-07 15:52:10 +0200

    Move definition of `configurable::configurable(nothrow_t)` to "configurable.hpp", as per https://travis-ci.org/nabijaczleweli/Cwepper/builds/65773455#L689

--- src/util/configurable.cpp
+++ src/util/configurable.cpp
@@ -32,4 +32,3 @@ configurable::configurable(bool do_load) : load(do_load) {
 		app_configuration.add(this);
 }
 configurable::configurable() : configurable(true) {}
-configurable::configurable(nothrow_t) : configurable(false) {}
